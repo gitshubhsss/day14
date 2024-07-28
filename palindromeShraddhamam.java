472d093f6ed810f313a4052720293ff1e7dd348e
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-28 22:47:55 +0530

    .\palindromeShraddhamam.java

---
+++ palindromeShraddhamam.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class palindromeShraddhamam {
+    public static boolean checkPalindrome(String str){
+        int n=str.length();
+
+        for(int i=0;i<n/2;i++){
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string");
+        String str=sc.nextLine();
+
+        System.out.println(checkPalindrome(str));
+    }
+}
