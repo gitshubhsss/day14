3ecb849de8406dd762770063729dd6a6df63b83c
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-28 22:47:36 +0530

    anaram

---
+++ anaram.java
@@ -0,0 +1,12 @@
+public class anaram {
+    public static void main(String[] args) {
+        String str1="earth";
+        String str2="heart";
+        int  freq[]=new int  [26];
+        char a='a';
+        for(int i=0;i<str1.length();i++){
+            char ch=str1.charAt(i);
+            freq[ch-a];
+        }
+    }
+}
